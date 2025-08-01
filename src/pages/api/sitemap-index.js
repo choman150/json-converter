3ede4c36b4ed8527d7dc8cadb9da53c5191b3155
choman150 <choman150@amoeba.co.kr>
2025-08-01 15:13:51 +0900

    sitemap-index

---
+++ src/pages/api/sitemap-index.js
@@ -0,0 +1,45 @@
+export default function handler(req, res) {
+    // 로그 찍기 (Vercel 로그에서 확인 가능)
+    console.log("✅ [SITEMAP-INDEX] Request received:", {
+        method: req.method,
+        ua: req.headers["user-agent"],
+        ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    });
+
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemap>
+      <loc>https://choman150.click/</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/about</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/privacy-policy</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/contact</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/how-to</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/faq</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+    <sitemap>
+      <loc>https://choman150.click/flattening-json</loc>
+      <lastmod>2025-07-31</lastmod>
+    </sitemap>
+  </sitemapindex>`;
+
+    res.status(200).send(sitemap);
+}
