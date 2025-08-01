c8dce27fa34abe959f3838eee0dc6809cfd6a2fd
choman150 <choman150@amoeba.co.kr>
2025-08-01 16:50:56 +0900

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/sitemap.xml",
+                headers: [
+                    {
+                        key: "Content-Type",
+                        value: "application/xml; charset=utf-8",
+                    },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "*",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
