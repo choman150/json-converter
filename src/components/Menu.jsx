427463585009bbc4fe8c267eaa967fb56b9ba8a1
choman150 <choman150@amoeba.co.kr>
2025-08-04 11:01:18 +0900

    ad first

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,35 @@
 import Link from "next/link";
+import Script from "next/script";
 
 export default function Menu() {
     return (
-        <nav className="mb-8 bg-gray-100 rounded px-4 py-3 shadow flex gap-6 text-sm font-medium text-blue-700 justify-center">
-            <Link href="/" className="hover:underline hover:text-blue-900">
-                🏠 Home
-            </Link>
-            <Link href="/about" className="hover:underline hover:text-blue-900">
-                📘 About
-            </Link>
-            <Link href="/how-to" className="hover:underline hover:text-blue-900">
-                🛠️ How To
-            </Link>
-            <Link href="/faq" className="hover:underline hover:text-blue-900">
-                ❓ FAQ
-            </Link>
-            <Link href="/flattening-json" className="hover:underline hover:text-blue-900">
-                🧾 JSON Flattening
-            </Link>
-        </nav>
+        <>
+            <div id="ezoic-pub-ad-placeholder-118"></div>
+            <Script>
+                {`
+    ezstandalone.cmd.push(function () {
+        ezstandalone.showAds(118);
+    });
+    `}
+            </Script>
+
+            <nav className="mb-8 bg-gray-100 rounded px-4 py-3 shadow flex gap-6 text-sm font-medium text-blue-700 justify-center">
+                <Link href="/" className="hover:underline hover:text-blue-900">
+                    🏠 Home
+                </Link>
+                <Link href="/about" className="hover:underline hover:text-blue-900">
+                    📘 About
+                </Link>
+                <Link href="/how-to" className="hover:underline hover:text-blue-900">
+                    🛠️ How To
+                </Link>
+                <Link href="/faq" className="hover:underline hover:text-blue-900">
+                    ❓ FAQ
+                </Link>
+                <Link href="/flattening-json" className="hover:underline hover:text-blue-900">
+                    🧾 JSON Flattening
+                </Link>
+            </nav>
+        </>
     );
 }
