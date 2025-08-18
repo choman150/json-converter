3668126cba45984c403f21f77d9259e4c302b346
choman150 <choman150@amoeba.co.kr>
2025-08-18 14:22:36 +0900

    ...

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,7 +6,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <meta name="monetag" content="ca8f15006cbbacfd5946ee303eb736ce" />
-                <script src="inpagepush-loader.js" />
+                <script data-cfasync="false" type="text/javascript" src="inpagepush-loader.js" />
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `(function(d,z,s,c){s.src='//'+d+'/400/'+z;s.onerror=s.onload=E;function E(){c&&c();c=null}try{(document.body||document.documentElement).appendChild(s)}catch(e){E()}})('niwooghu.com',9732874,document.createElement('script'),_rocxgvr)`,
