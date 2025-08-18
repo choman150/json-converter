ef8a42daa6eb6ebfb0c5d2e745deffacd179834d
choman150 <choman150@amoeba.co.kr>
2025-08-18 14:45:39 +0900

    ...

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,16 +6,14 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <meta name="monetag" content="ca8f15006cbbacfd5946ee303eb736ce" />
-                {/* <script data-cfasync="false" type="text/javascript" src="inpagepush-loader.js" />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `(function(d,z,s,c){s.src='//'+d+'/400/'+z;s.onerror=s.onload=E;function E(){c&&c();c=null}try{(document.body||document.documentElement).appendChild(s)}catch(e){E()}})('niwooghu.com',9732874,document.createElement('script'),_rocxgvr)`,
-                    }}
-                ></script> */}
-
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9732874,document.createElement('script'))`,
+                    }}
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9733051,document.createElement('script'))`,
                     }}
                 ></script>
             </Head>
